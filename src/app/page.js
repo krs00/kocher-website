4985cc423a20bb5b3d474d0ac5bbce2441abdaf7
Kymani Stephens <kymanist00@gmail.com>
2023-12-05 18:51:10 -0800

    WIP: video player

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,23 @@
 export default function Home() {
   return (
     <>
-    <h1 className="page-header">The Kocher Scandal</h1>
-    <h2 className="page-subheader">A True Crime Timeline</h2>
+      <h1 className="page-header">The Kocher Scandal</h1>
+      <h2 className="page-subheader">A True Crime Timeline</h2>
+
+      <video width="640" height="360" controls>
+        <source src="/videos/office.mp4" type="mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <p className="video-subtitle">How did the footage come to be?</p>
+      <p className="video-subtitle video-subtitle2">Follow the timeline to review evidence</p>
+
     </>
 
 
     //<main className={styles.main}>
     //<h1>hello</h1>
-      
+
     // </main>
   )
 }
